9f1371ec70940332afbe5daab2cd9d0bd6feb3d9
zhengkai <912198227@qq.com>
2017-09-15 16:21:30 +0800

    cordova-test

--- js/abc.js
+++ js/abc.js
@@ -4,3 +4,17 @@ document.querySelector("#test").onclick=function () {
 document.querySelector("#test2").onclick=function () {
     alert('被点了')
 }
+document.querySelector("#test3").addEventListener("click",scanCode)
+function scanCode() {
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        }
+    );
+}
